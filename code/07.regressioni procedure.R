aaa5617441d2293071d5681dd9b8516590fdea96
Mariagrazia Cicala <mariagraziacicala@AirdiMaiagrazia.homenet.telecomitalia.it>
2023-06-15 13:17:56 +0200

    gtsummary

--- code/07.regressioni procedure.R
+++ code/07.regressioni procedure.R
@@ -1,7 +1,10 @@
 #Regressione univariata
 
-regr_uni <- glm(infetto ~ proc_inv_re, 
+regr_uni <- glm(infetto ~ proc_inv, 
                data = db,
                family = binomial("logit"))
 
 summary(regr_uni)
+
+gtsummary::tbl_regression(regr_uni)
+
