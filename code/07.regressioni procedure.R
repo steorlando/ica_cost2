436c8d174394041c0e633190a4509a9ebcdc09b7
Mariagrazia Cicala <mariagraziacicala@AirdiMaiagrazia.homenet.telecomitalia.it>
2023-06-15 14:30:51 +0200

    Regressione multivariata

--- code/07.regressioni procedure.R
+++ code/07.regressioni procedure.R
@@ -8,3 +8,14 @@ summary(regr_uni)
 
 gtsummary::tbl_regression(regr_uni)
 
+
+#Regressione multivariata
+regr_multi <- glm(
+  infetto ~ sdo1_eta + sdo1_modali + sdo1_degenza + terapia + decessodico + reparto + dia_pri + proc_inv, 
+  data = db,
+  family = binomial("logit")
+)
+
+summary(regr_multi)
+
+tbl_regression(regr_multi)
